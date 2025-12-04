0d5aa5a3e638db33f9f649dc41b27de3f008c68d
AZDEVELOBERRB <ft47fc@gmail.com>
2025-12-04 17:21:44 +0300

    f

---
+++ src/lib.rs
@@ -0,0 +1,7 @@
+
+/// contains all request functionalities
+pub mod request;
+
+mod utils;
+/// defining important http configurations
+pub mod config;
--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,5 @@
-use water_http_utils::request::HttpRequest;
 
 fn main() {
 
-    let re = b"GET /path?q=2&a=1&2=2&&ss=2 HTTP/1.1\r\nHost: example.com\r\nUser-Agent: MyClient/1.0\r\nAccept: */*\r\n\r\n";
 
-    let req = HttpRequest::<16>::from_incoming_bytes::<16>(re);
-    if let Ok( req ) = req {
-        let path = req.path();
-        println!("{:?}",path.to_query());
-    }
 }
